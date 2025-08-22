7f39f7b19e1c84a83ecfa3da046cd7d1be4bf6b0
Tetlanyo Moitse <tetlanyomoitse1@gmail.com>
2025-08-23 00:34:49 +0200

    so like it's working now [i think]

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Contact(db.Model):
-    id = db.Column(db.Integer, primary_keys=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120))
     phone = db.Column(db.String(20))
